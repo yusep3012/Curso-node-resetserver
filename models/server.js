e9ba450983c551a3e5d2d484c0f62077982fc202
Yusep <yusep3012@hotmail.com>
2023-02-23 02:57:52 -0500

    Ignorado .env y agregado puerto por defecto

--- models/server.js
+++ models/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 4000;
     this.userPath = "/api/usuarios";
 
     // Middlewares
